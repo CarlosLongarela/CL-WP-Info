9738d5ce490aa823e488c0ade0a520aee2f3e6f7
Carlos Longarela <carlos@longarela.eu>
2017-02-13 10:16:31 +0100

    Versión Inicial

---
+++ cl-functions.php
@@ -0,0 +1,260 @@
+<?php
+/**
+ *
+ * Description: Funciones para devolver info de PHP, WordPress.
+ *
+ * @package CL WordPress Info
+ * License: GPL2+
+ * Text Domain: cl-wp-info
+ */
+
+/**
+ * Devuelve caracterísitcas del Servidor.
+ *
+ * @since     1.0.0
+ */
+function cl_wp_server_info() {
+	$html  = '';
+
+	$html .= '<tr>';
+	$html .= '<th>' . __( 'OS Server:', 'cl-wp-info' ) . '</th>';
+	$html .= '<td>' . php_uname() . '</td>';
+	$html .= '</tr>';
+
+	if ( ! empty( $_SERVER['SERVER_SOFTWARE'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['SERVER_SOFTWARE'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $_SERVER['USER'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server User:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['USER'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $_SERVER['SERVER_NAME'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server Name:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['SERVER_NAME'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $_SERVER['SERVER_PORT'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server Port:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['SERVER_PORT'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $_SERVER['SERVER_ADDR'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server Ip:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['SERVER_ADDR'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $_SERVER['REQUEST_SCHEME'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server Scheme:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['REQUEST_SCHEME'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $_SERVER['SERVER_PROTOCOL'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server Http Version:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['SERVER_PROTOCOL'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $_SERVER['DOCUMENT_ROOT'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Web Server Root:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . $_SERVER['DOCUMENT_ROOT'] . '</td>';
+		$html .= '</tr>';
+	}
+
+	return $html;
+} // Final de cl_wp_server_info.
+
+/**
+ * Devuelve caracterísitcas de PHP.
+ *
+ * @since     1.0.0
+ */
+function cl_wp_php_info() {
+	$html  = '';
+
+	$html .= '<tr>';
+	$html .= '<th>' . __( 'PHP Version:', 'cl-wp-info' ) . '</th>';
+	$html .= '<td>' . phpversion() . '</td>';
+	$html .= '</tr>';
+
+	$html .= '<tr>';
+	$html .= '<th>' . __( 'PHP Loaded Extensions:', 'cl-wp-info' ) . '</th>';
+	$html .= '<td>';
+	$php_extensions = get_loaded_extensions();
+	foreach ( $php_extensions as &$valor ) {
+		$html .= $valor . ', ';
+	}
+	$html .= '</td>';
+	$html .= '</tr>';
+
+	$html .= '<tr>';
+	$html .= '<th>' . __( 'PHP Webserver Interface:', 'cl-wp-info' ) . '</th>';
+	$html .= '<td>' . php_sapi_name() . '</td>';
+	$html .= '</tr>';
+
+	$php_ini = ini_get_all();
+
+	if ( ! empty( $php_ini['date.timezone'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Date/Time Zone:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>';
+		$html .= '<div><strong>' . __( 'PHP Global:', 'cl-wp-info' ) . '</strong> ' . $php_ini['date.timezone']['global_value'] . '</div>';
+		$html .= '<div><strong>' . __( 'App Local:', 'cl-wp-info' ) . '</strong> ' . $php_ini['date.timezone']['local_value'] . '</div>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $php_ini['error_log'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Error Log:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>';
+		$html .= '<div><strong>' . __( 'PHP Global:', 'cl-wp-info' ) . '</strong> ' . $php_ini['error_log']['global_value'] . '</div>';
+		$html .= '<div><strong>' . __( 'App Local:', 'cl-wp-info' ) . '</strong> ' . $php_ini['error_log']['local_value'] . '</div>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $php_ini['max_file_uploads'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Max File Uploads:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>';
+		$html .= '<div><strong>' . __( 'PHP Global:', 'cl-wp-info' ) . '</strong> ' . $php_ini['max_file_uploads']['global_value'] . '</div>';
+		$html .= '<div><strong>' . __( 'App Local:', 'cl-wp-info' ) . '</strong> ' . $php_ini['max_file_uploads']['local_value'] . '</div>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $php_ini['post_max_size'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Post Max Size:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>';
+		$html .= '<div><strong>' . __( 'PHP Global:', 'cl-wp-info' ) . '</strong> ' . $php_ini['post_max_size']['global_value'] . '</div>';
+		$html .= '<div><strong>' . __( 'App Local:', 'cl-wp-info' ) . '</strong> ' . $php_ini['post_max_size']['local_value'] . '</div>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $php_ini['upload_max_filesize'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Upload Max File Size:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>';
+		$html .= '<div><strong>' . __( 'PHP Global:', 'cl-wp-info' ) . '</strong> ' . $php_ini['upload_max_filesize']['global_value'] . '</div>';
+		$html .= '<div><strong>' . __( 'App Local:', 'cl-wp-info' ) . '</strong> ' . $php_ini['upload_max_filesize']['local_value'] . '</div>';
+		$html .= '</tr>';
+	}
+
+	if ( ! empty( $php_ini['memory_limit'] ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Max Memory:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>';
+		$html .= '<div><strong>' . __( 'PHP Global:', 'cl-wp-info' ) . '</strong> ' . $php_ini['memory_limit']['global_value'] . '</div>';
+		$html .= '<div><strong>' . __( 'App Local:', 'cl-wp-info' ) . '</strong> ' . $php_ini['memory_limit']['local_value'] . '</div>';
+		$html .= '</tr>';
+	}
+	return $html;
+} // Final de cl_wp_php_info.
+
+/**
+ * Devuelve caracterísitcas de WordPress.
+ */
+function cl_wp_wordpress_info() {
+	$html  = '';
+
+	$html .= '<tr>';
+	$html .= '<th>' . __( 'WordPress Version:', 'cl-wp-info' ) . '</th>';
+	$html .= '<td>' . get_bloginfo('version') . '</td>';
+	$html .= '</tr>';
+
+	$args = array( 'errors' => false , 'allowed' => null );
+	$temas = wp_get_themes( $args );
+
+	//$tema_actual = wp_get_theme();
+
+	$html .= '<tr>';
+	$html .= '<th>' . __( 'Themes:', 'cl-wp-info' ) . '</th>';
+	$html .= '<td>';
+	foreach ( $temas as &$valor ) {
+		$html .= '<div>' . $valor . '</div>';
+		//$html .= $valor->headers:WP_Theme:private['Version'];
+	}
+	$html .= '</td>';
+	$html .= '</tr>';
+
+	$html .= '<tr>';
+	$html .= '<th>' . __( 'Plugins:' ) . '</th>';
+	$html .= '<td>';
+	$plugins         = get_plugins();
+	$plugins_activos = get_option( 'active_plugins', array () );
+	foreach ( $plugins as $clave => $valor) {
+		$html .= '<div>' . $valor['Name'] . ' <em>(' . __( 'Version' ) . ': ' . $valor['Version'] . ')</em>';
+
+		if ( false !== array_search( $clave, $plugins_activos ) ) {
+			$html .= ' <strong>' . __( 'Active', 'cl-wp-info' ) . '</strong>';
+		}
+
+		$html .= '</div>';
+	}
+	$html .= '</td>';
+	$html .= '</tr>';
+
+	if ( defined( 'ABSPATH' ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'Absolute Path to WordPress Directory:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . ABSPATH . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( defined( 'WP_CONTENT_DIR' ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'WordPress Content Directory:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . WP_CONTENT_DIR . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( defined( 'DB_NAME' ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'WordPress Database:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . DB_NAME . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( defined( 'WP_HOME' ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'WordPress Home:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . WP_HOME . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( defined( 'WP_SITEURL' ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'WordPress URL:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . WP_SITEURL . '</td>';
+		$html .= '</tr>';
+	}
+
+	if ( defined( 'WP_DEBUG' ) ) {
+		$html .= '<tr>';
+		$html .= '<th>' . __( 'WordPress Debug:', 'cl-wp-info' ) . '</th>';
+		$html .= '<td>' . WP_DEBUG . '</td>';
+		$html .= '</tr>';
+	}
+
+/*
+echo '<pre>';
+print_r(ini_get_all());
+echo '</pre>';
+*/
+	return $html;
+} // Final de cl_wp_wordpress_info.
---
+++ cl-wp-info.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Plugin Name: CL WordPress Info
+ * Plugin URI: https://desarrolloweb.longarela.eu/
+ * Description: Información del servidor, PHP y plugins y temas de WordPress para poder realizar fácilmente un infrme previo de la web sobre la que vamos a trabajar
+ * Version: 1.0
+ * Author: Carlos Longarela
+ * Author URI: https://desarrolloweb.longarela.eu/
+ *
+ * @package CL WordPress Info
+ * License: GPL2+
+ * Text Domain: cl-wp-info
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/**
+ * Registramos el menú.
+ *
+ * @since     1.0.0
+ */
+function cl_wp_info_add_menu_page() {
+	add_menu_page(
+		__( 'CL Info WP', 'cl-wp-info' ),
+		__( 'Info WP', 'cl-wp-info' ),
+		'manage_options',
+		'cl-wp-info',
+		'cl_wp_info_general',
+		'dashicons-visibility',
+		3
+	);
+}
+add_action( 'admin_menu', 'cl_wp_info_add_menu_page' );
+
+/**
+ * Carga de archivo CSS sólo en esta página de admin.
+ *
+ * @since     1.0.0
+ * @param     string $hook   El hook (gancho) de la página actual.
+ */
+function load_custom_wp_admin_style( $hook ) {
+	// Cargar solo en ?page=toplevel_page_cl-wp-info.
+	if ( 'toplevel_page_cl-wp-info' !== $hook ) {
+			return;
+	}
+	wp_enqueue_style( 'custom_wp_admin_css', plugins_url( 'css/cl-wp-info-admin.min.css', __FILE__ ) );
+}
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
+/**
+ * Carga inicial en el plugin de traducciones.
+ *
+ * @since     1.0.0
+ */
+function cl_wp_info_init() {
+	load_plugin_textdomain( 'cl-wp-info', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
+add_action( 'plugins_loaded', 'cl_wp_info_init' );
+
+// Incluimos el archivo con las funciones de info del sistema y WP.
+require_once plugin_dir_path( __FILE__ ) . 'cl-functions.php';
+
+/**
+ * Función principal encargada de mostrar toda la infomación.
+ *
+ * @since     1.0.0
+ */
+function cl_wp_info_general() {
+	echo '<table class="cl-tabla-general">';
+
+	echo '<tbody>';
+	echo '<tr><th colspan="2">' . __( 'Server Info', 'cl-wp-info' ) . '</tr></th>';
+	echo cl_wp_server_info();
+	echo '</tbody>';
+
+	echo '<tbody>';
+	echo '<tr><th colspan="2">' . __( 'PHP Info', 'cl-wp-info' ) . '</tr></th>';
+	echo cl_wp_php_info();
+	echo '</tbody>';
+
+	echo '<tbody>';
+	echo '<tr><th colspan="2">' . __( 'WordPress Info', 'cl-wp-info' ) . '</tr></th>';
+	echo cl_wp_wordpress_info();
+	echo '</tbody>';
+
+	echo '</table>';
+}
+
+//echo 'post_max_size = ' . ini_get('post_max_size');
